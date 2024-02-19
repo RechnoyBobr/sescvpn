cce24cef228ffb8c9eaefe3a47d18c52dd2bb4b7
RechnoyBobr <someshitcoding@list.ru>
2024-02-19 19:04:33 +0300

    Finished form and post request

--- frontend/app/form.jsx
+++ frontend/app/form.jsx
@@ -1,10 +1,20 @@
+"use client"
 export default function Form() {
+  async function handler(e) {
+    e.preventDefault();
+    const form = e.target;
+    const Data = new FormData(form);
+    console.log(Data);
+    // const res = await fetch('http://localhost:{ServerPort}/make-users', { method: "POST", body: JSON.stringify(Data) });
+    // const product = res.json(); // Json with field config containing finished unique config for user 
+    // TODO: output config
+  }
   return (
-    <form action="/make-user" className="flex gap-5 flex-col align-center" method="POST">
+    // TODO: Add checkbox for reverse proxy
+    <form className="flex gap-5 flex-col align-center" method="POST" onSubmit={handler}>
       <input className="text-black w-1/4 justify-self-center" type="text" placeholder="Input e-mail" name="mail" />
       <input className="w-1/4 text-black" type="text" placeholder="Input name" name="login" />
-      <button type="submit">bebra</button>
-    </form>
+      <button type="submit">Submit</button>
+    </form >
   )
-
 }
