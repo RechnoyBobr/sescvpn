d245d7e1ffed5b8be3bc975f0455a8a6dcd66a0c
RechnoyBobr <someshitcoding@list.ru>
2024-03-10 23:48:59 +0300

    Implemented xray server response for POST request

--- frontend/app/form.jsx
+++ frontend/app/form.jsx
@@ -7,7 +7,7 @@ export default function Form() {
     const form = e.target;
     const Data = new FormData(form);
     console.log(Data);
-    const res = await fetch('http://django:5000', { method: "POST", body: JSON.stringify(Data) });
+    const res = await fetch('http://backend:5000', { method: "POST", body: JSON.stringify(Data) });
     const product = res.json(); // Json with field config containing finished unique config for user 
     setData(product)
     // TODO: output config
@@ -16,10 +16,8 @@ export default function Form() {
     // TODO: Add checkbox for reverse proxy
     <div>
       <form className="flex gap-5 flex-col align-center" method="POST" onSubmit={handler}>
-        <input className="text-black w-1/4 justify-self-center" type="text" placeholder="Input e-mail" name="mail" />
+        <input className="text-black w-1/4 justify-self-center" type="text" placeholder="Input e-mail" name="email" />
         <input className="w-1/4 text-black" type="text" placeholder="Input name" name="login" />
-        <input type="checkbox" name="is-reverse" id="check" />
-        <label for="check">Reverse proxy</label>
         <button type="submit">Submit</button>
       </form >
       <div>{data}</div>
