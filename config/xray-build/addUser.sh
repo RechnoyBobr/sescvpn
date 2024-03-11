76eb10b70193b2b50312740ffb356e0d1bb9335a
RechnoyBobr <someshitcoding@list.ru>
2024-03-11 06:08:26 +0300

    Finished 1 MVP stage of the project

--- config/xray-build/addUser.sh
+++ config/xray-build/addUser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 UUID=$(eval "xray uuid")
 echo $UUID >/uuid
-sed "/clients\" : \[.*/a\ {\"id\": \"$UUID\"}," /etc/xray/config.json >/etc/xray/temp.json
-perl -MJSON -e '@text=(<>);print to_json(from_json("@text", {relaxed=>1}), {pretty=>1})' ./etc/xray/temp.json >/etc/xray/config.json
+sed "/clients\" : \[.*/a\ {\"id\": \"$UUID\", \"flow\": \"xtls-rprx-vision\"}," /etc/xray/config.json >/etc/xray/temp.json
+perl -MJSON -e '@text=(<>);print to_json(from_json("@text", {relaxed=>1}), {pretty=>1})' /etc/xray/temp.json >/etc/xray/config.json
 rm /etc/xray/temp.json
